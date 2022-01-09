f5ac338ea1a179cb182c0b4fd16477c36df62318
hoangvictor <41613226+hoangvictor@users.noreply.github.com>
2022-01-09 18:13:56 +0700

    Update stockmarket.py

--- stockmarket.py
+++ stockmarket.py
@@ -6,14 +6,16 @@ from time import sleep
 
 def test_sendIMG(update, context):
     update.message.reply_text('HI')
-    filename = "stock_market_chart.jpg"
+    filename = "stock_market_chart.png"
     driver = webdriver.Firefox()
     driver.get('https://ceilingstock.vn/chart.html?symbol=HNG')
-    sleep(1)
+    sleep(0.5)
 
     driver.get_screenshot_as_file(filename)
 
     update.message.bot.send_photo(
         update.message.chat.id, open(filename, "rb"))
+
+    driver.quit()
 
     os.remove(filename)
